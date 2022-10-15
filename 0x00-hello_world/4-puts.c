7e32b7855d5d34a623305d770e8c220332449812
root <princeukaegbu4>
2022-10-15 12:25:06 -0700

    my fifth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-nclude < stdio.h >
+#include <stdio.h>
 /**
  *  *main- Serves as the entry point for the program
- *  *
- *  *Return:By default return zero if no erros otherwise returns non-zero value
- *
+ *   *
+ *  *Return:By default return zero if no erros otherwise return non-zero value
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
+
