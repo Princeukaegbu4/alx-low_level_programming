ff7ea21771bf95b8e81a8cc4ac5b6e1e841d7e31
root <princeukaegbu4>
2022-10-15 22:31:18 -0700

    my seventh commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,9 @@
 */
 int main(void)
 {
-printf("Size of a char: %ld byte(s)\n", sizeof(char));
-printf("Size of an int: %ld byte(s)\n", sizeof(int))
-pintf("Size of a long int: %ld byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
-return (0);
+   printf("Size of a char: %ld byte(s)\n", sizeof(char));
+   printf("Size of an int: %ld byte(s)\n", sizeof(int))
+   pintf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+   printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+   return (0);
 }
